bcc693075891b7590216cc0fe2aa31a426dc212d
josephcoombes <josephcoombes@me.com>
2014-08-06 23:13:18 +0000

    Finished Part 1

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -28,4 +28,4 @@ class Movie < ActiveRecord::Base
     end
   end
 
-end
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,26 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
+  # get 'reviews/new'
 
-  get 'sessions/create'
+  # get 'reviews/create'
 
-  get 'users/new'
+  # get 'sessions/new'
 
-  get 'users/create'
+  # get 'sessions/create'
+
+  # get 'users/new'
+
+  # get 'users/create'
 
 RottenMangoes::Application.routes.draw do
-  get 'sessions/new'
 
-  get 'sessions/create'
-
-  get 'users/new'
-
-  get 'users/create'
-
-  resources :sessions, only: [:new, :create]
-
-  resources :movies
+  resources :movies do
+    resources :reviews, only: [:new, :create]
+  end
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  root to: 'movies#index'
+
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
